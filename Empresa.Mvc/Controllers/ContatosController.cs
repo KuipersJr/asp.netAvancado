d89c7e7ca6e61a4d9575854578126009b8430493
Kuipersjr <kuipersjr@gmail.com>
2017-10-07 17:42:56 -0300

    teste

---
+++ Empresa.Mvc/Controllers/ContatosController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Empresa.Repositorio.SqlServer;
+using Empresa.Dominio;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Empresa.Mvc.Controllers
+{
+    public class ContatosController : Controller
+    {
+        private readonly EmpresaDbContex _contexto;
+        public ContatosController(EmpresaDbContex contexto)
+        {
+            _contexto = contexto;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(_contexto.Contatos.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Create(Contato contato)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(contato);
+            }
+
+            _contexto.Add(contato);
+            _contexto.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Empresa.Repositorio.SqlServer/EmpresaDbContex.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Empresa.Dominio;
+
+namespace Empresa.Repositorio.SqlServer
+{
+    public class EmpresaDbContex : DbContext
+    {
+        public EmpresaDbContex(DbContextOptions options) : base(options)
+        {
+
+            Database.EnsureCreated();
+        }
+        public DbSet<Contato> Contatos  { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
