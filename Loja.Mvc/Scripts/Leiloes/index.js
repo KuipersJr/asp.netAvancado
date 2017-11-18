aed522c5bd400032f1c4646cda4bf31dc9c35f03
Kuipersjr <kuipersjr@gmail.com>
2017-11-18 11:16:55 -0200

    knockout

---
+++ Loja.Mvc/Scripts/Leiloes/index.js
@@ -0,0 +1,28 @@
+﻿var Index = {
+    viewModel: {
+        produtos: ko.observableArray()
+    },
+
+    inicializar: function () {
+        this.connectarLeilaohub();
+        ko.applyBindings(this.viewModel);
+    },
+
+    connectarLeilaohub: function () {        
+        var connection = $.hubConnection();
+        var hub = connection.createHubProxy("LeilaoHub");
+
+        hub.on("atualizarOfertas", this.atualizarOfertas.bind(this) );
+        connection.start();
+    },
+
+    atualizarOfertas: function() {
+        this.viewModel.produtos.push({
+            id: 1,
+            nome: "Teste",
+            preco: 11.50,
+            estoque: 10,
+            categorianome: "Papelaria"
+        });
+    } 
+};
