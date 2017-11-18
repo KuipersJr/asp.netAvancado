1dffc2dd66a61dbe9e08c2463f8e20d85f4aabef
Kuipersjr <kuipersjr@gmail.com>
2017-11-18 16:51:32 -0200

    Test Unitario

---
+++ Loja.Dominio/Leilao.cs
@@ -0,0 +1,42 @@
+﻿using Loja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Dominio
+{
+    public class Leilao
+    {
+        public const decimal DescontoMaximo = 0.1m;
+        public int Id { get; set; }
+        public string NomeLote { get; set; }
+        public decimal Preco { get; set; }
+        public List<Produto> Produtos { get; set; }
+
+        public List<string> validar()
+        {
+            var erros = new List<string>();
+
+            if (string.IsNullOrEmpty(NomeLote?.Trim()))
+            {
+                erros.Add("Nome vazio");
+            }
+
+            var somaProdutos = Produtos.Sum(p => p.Preco);            
+
+            if ((somaProdutos - Preco) > (somaProdutos * DescontoMaximo))
+            {
+                erros.Add("Desconto máximo excedido");
+            }
+
+            if(Produtos.Count <= 0)
+            {
+                erros.Add("Nenhum produtos selecionado");
+            }
+
+            return erros;
+        }
+    }
+}
---
+++ Loja.DominioTests/LeilaoTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Loja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Dominio.Tests
+{
+    [TestClass()]
+    public class LeilaoTests
+    {
+        [TestMethod()]
+        public void validarSucessoTest()
+        {
+            // Arrange
+            var leilao = new Leilao();
+            leilao.Id = 1;
+            leilao.NomeLote = "Podutos";
+            leilao.Preco = 100.50m;
+            leilao.Produtos = 
+                new List<Produto> { new Produto { Preco = 100m} };
+
+            // Act
+
+            var erros = leilao.validar();
+
+            // Assert 
+
+            Assert.IsTrue(erros.Count == 0);
+        }
+
+        [TestMethod()]
+        public void validarErroTest()
+        {
+            // Arrange
+            var leilao = new Leilao();
+            leilao.Id = 1;
+            leilao.NomeLote = "Podutos";
+            leilao.Preco = 50.50m;
+            leilao.Produtos =
+                new List<Produto> { new Produto { Preco = 100m } };
+
+            // Act
+
+            var erros = leilao.validar();
+
+            // Assert 
+
+            Assert.IsTrue(erros.Contains("Desconto máximo excedido"));
+        }
+    }
+}
